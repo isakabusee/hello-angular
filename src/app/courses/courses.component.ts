917bfd82555e822c2f6e291b80c43392b36332cd
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2021-02-03 15:49:53 -0800

    event handling

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { CoursesService } from '../courses.service';
+// import { CoursesService } from '../courses.service';
 
 @Component({
   selector: 'courses',
   template: `
-    <button class="btn btn-primary" [class.active]="isActive">Save</button>
+  <div (click)="onDivClicked()">
+    <button (click)="onSave($event)">Save</button>
+  </div>
   `
+  
   //this section is called the DOM
 })
 export class CoursesComponent {
-  isActive = true;
-  // title = "List of courses";
-  // imageUrl = ""
-  //this section is called as the component
+  onDivClicked() {
+    console.log("Div was clicked")
+  }
+  onSave($event) {
+    $event.stopPropagation();
+    console.log("This button was clicked", $event);
+  }
 }
