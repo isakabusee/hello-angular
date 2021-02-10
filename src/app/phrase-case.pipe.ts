1f68612f06e5ec56dc0bfb3122d746305c40127e
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2021-02-10 00:37:43 -0800

    modularized code

--- src/app/phrase-case.pipe.ts
+++ src/app/phrase-case.pipe.ts
@@ -8,10 +8,31 @@ export class PhraseCasePipe implements PipeTransform {
   transform(value: string): any {
     if (!value) return null;
 
-    let words = value.splint(' ');
+    
+
+    let words = value.split(' ');
     for (var i = 0; i < words.length; i++) {
+      let word = words[i];
+      if (i > 0 && this.isPreposition(word))
+      words[i] = word.toLowerCase();
+      else 
+        words[i] = this.toPhraseCase(word);
       
+
     }
+    return words.join(' ');
+  }
+
+  private toPhraseCase(word: string): string {
+    return word.substr(0, 1).toUpperCase() + word.substr(1).toLowerCase();
+  }
+  private isPreposition(word: string): boolean {
+    let prepositions = [
+      'of',
+      'the'
+
+    ];
+    return prepositions.includes(word.toLowerCase())
   }
 
 }
